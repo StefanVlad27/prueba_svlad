4c270a32ed95ee0c025ff96b897bc5241fcc69ec
stefanvlad27 <stefan.infor11@gmail.com>
2023-07-06 15:40:18 +0200

    cambio

--- SQL/dbo_usp_Download_Validation_UNDO.sql
+++
@@ -1,6 +0,0 @@
-﻿USE [CINCAPORC_DW]
-GO
-
-/****** Object:  StoredProcedure [dbo].[usp_Download_Validation]    Script Date: 20/03/2023 16:23:15 ******/
-DROP PROCEDURE [dbo].[usp_Download_Validation]
-GO
